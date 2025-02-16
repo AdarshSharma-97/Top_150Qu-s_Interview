ff16b805313831f49e4033df3b10138f049f665e
Adarsh Sharma <adarshs861@gmail.com>
2025-02-16 17:28:50 +0530

    Question 20

---
+++ 20.ReverseWordsinaString.js
@@ -0,0 +1,17 @@
+var reverseWords = function (s) {
+  let n = s.length;
+  let trimmed = "";
+  let i = 0;
+
+  while (i < n) {
+    while (i < n && s[i] === " ") i++;
+    if (i >= n) break;
+
+    let start = i;
+    while (i < n && s[i] !== " ") i++;
+    if (trimmed.length > 0) trimmed = " " + trimmed;
+    trimmed = s.slice(start, i) + trimmed;
+  }
+
+  return trimmed;
+};
