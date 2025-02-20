1707fc33a2ef0c1e634dec42d9107230201b4e5e
Adarsh Sharma <adarshs861@gmail.com>
2025-02-20 10:04:27 +0530

    Question 24

---
+++ 24.IsSubsequence.js
@@ -0,0 +1,19 @@
+const isSubsequence = (s, t) => {
+  // base case
+  if (s.length > t.length) return false;
+  if (!s) return true;
+
+  let i = 0,
+    j = 0;
+
+  while (i < s.length && j < t.length) {
+    if (s[i] == t[j]) {
+      i++;
+      j++;
+    } else {
+      j++;
+    }
+  }
+
+  return i === s.length;
+};
