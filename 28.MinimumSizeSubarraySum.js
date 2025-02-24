de766cbbc32279c89cb88350aa5ff2e6262c326a
Adarsh Sharma <adarshs861@gmail.com>
2025-02-24 22:43:37 +0530

    Question 28

---
+++ 28.MinimumSizeSubarraySum.js
@@ -0,0 +1,17 @@
+var minSubArrayLen = function (s, nums) {
+  let windowSum = 0;
+  let output = Infinity;
+  let windowStart = 0;
+  for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+    windowSum += nums[windowEnd];
+    // shrink the window until the windowSum is smaller than s
+    while (windowSum >= s) {
+      output = Math.min(output, windowEnd - windowStart + 1);
+      // subtract the element at the windowStart index
+      windowSum -= nums[windowStart];
+      // change windowStart to the next element
+      windowStart++;
+    }
+  }
+  return output == Infinity ? 0 : output;
+};
