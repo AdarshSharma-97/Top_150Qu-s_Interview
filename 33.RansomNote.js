7024c8f4c29367d5eedff6dff8664d2c39b9a494
Adarsh Sharma <adarshs861@gmail.com>
2025-03-03 17:37:20 +0530

    Question 3

---
+++ 33.RansomNote.js
@@ -0,0 +1,12 @@
+const canConstruct = (ransomNote, magazine) => {
+  if (!ransomNote) return true;
+  if (!magazine) return false;
+
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (!magazine.includes(ransomNote[i])) return false;
+
+    magazine = magazine.replace(ransomNote[i], "");
+  }
+
+  return true;
+};
