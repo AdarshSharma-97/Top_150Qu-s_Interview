7996ab26284b3e650bd28b9706a1268ab073b20b
Adarsh Sharma <adarshs861@gmail.com>
2025-04-07 23:56:45 +0530

    Question 63

---
+++ 63.ConstructBinaryTreefromInorderandPostorderTraversal.js
@@ -0,0 +1,34 @@
+var buildTree = function (inorder, postorder) {
+  return helper(
+    inorder,
+    postorder,
+    0,
+    inorder.length - 1,
+    postorder.length - 1
+  );
+};
+
+var helper = function (inorder, postorder, inStart, inEnd, postIndex) {
+  if (inStart > inEnd || postIndex < 0) return null;
+
+  var index = 0;
+  var root = new TreeNode(postorder[postIndex]);
+
+  for (var i = inStart; i <= inEnd; i++) {
+    if (inorder[i] === root.val) {
+      index = i;
+      break;
+    }
+  }
+
+  root.right = helper(inorder, postorder, index + 1, inEnd, postIndex - 1);
+  root.left = helper(
+    inorder,
+    postorder,
+    inStart,
+    index - 1,
+    postIndex - 1 - (inEnd - index)
+  );
+
+  return root;
+};
