1fe6af0a71da08beb513dd6382d2c315808408c0
Adarsh Sharma <adarshs861@gmail.com>
2025-04-18 22:52:34 +0530

    Question 73

---
+++ 73.BinaryTreeLevelOrderTraversal.js
@@ -0,0 +1,21 @@
+var levelOrder = function (root) {
+  if (!root) return [];
+  const queue = [];
+  const output = [];
+  queue.push(root);
+  while (queue.length) {
+    // Remove all the current nodes in the queue and add each node's children to the queue
+    const len = queue.length;
+    const row = [];
+    for (let i = 0; i < len; i++) {
+      const cur = queue.shift();
+      if (cur.left) queue.push(cur.left);
+      if (cur.right) queue.push(cur.right);
+      // Push the current node val to the row array
+      row.push(cur.val);
+    }
+    // Push the current row array into the output array
+    output.push(row);
+  }
+  return output;
+};
