b8784142ce72ea0be39c11801a4da7c93b0d1db7
Adarsh Sharma <adarshs861@gmail.com>
2025-04-22 22:41:05 +0530

    Question 77

---
+++ 77.ValidateBinarySearchTree.js
@@ -0,0 +1,17 @@
+function isValidBST(root) {
+  function validate(node, min, max) {
+    if (!node) {
+      return true;
+    }
+    if (
+      (min !== null && node.val <= min) ||
+      (max !== null && node.val >= max)
+    ) {
+      return false;
+    }
+    return (
+      validate(node.left, min, node.val) && validate(node.right, node.val, max)
+    );
+  }
+  return validate(root, null, null);
+}
