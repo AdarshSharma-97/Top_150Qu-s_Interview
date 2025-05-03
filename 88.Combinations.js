56e9ab657c81812b26cf81c42c58583b6c8b31ca
Adarsh Sharma <adarshs861@gmail.com>
2025-05-03 22:49:54 +0530

    Question 88

---
+++ 88.Combinations.js
@@ -0,0 +1,19 @@
+function combine(n, k) {
+  const result = [];
+
+  function backtrack(start, combination) {
+    if (combination.length === k) {
+      result.push([...combination]);
+      return;
+    }
+
+    for (let i = start; i <= n; i++) {
+      combination.push(i);
+      backtrack(i + 1, combination);
+      combination.pop();
+    }
+  }
+
+  backtrack(1, []);
+  return result;
+}
